a1834e0b00c02912ea7c9f35d63126363fb77113
Mahamadou828 <122121Ms192001>
2022-06-19 23:08:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from "@mui/material";
 import './App.css';
 
 function App() {
@@ -10,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Button>
+         Hello, i do nothing
+       </Button>
       </header>
     </div>
   );
